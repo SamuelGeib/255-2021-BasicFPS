7f418bc042b8263c47089921b5a8cd307d283ad2
Samuel <skepticalnarcissist@gmail.com>
2021-01-20 16:50:55 -0500

    First Person Shooter
    In class first person shooter project.
    1/20/2021

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 2;
+    public float mouseSensitivityX = 10;
+    public float mouseSensitivityY = 10;
+
+    private CharacterController pawn;
+    private Camera cam;
+
+    float cameraPitch = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pawn = GetComponent<CharacterController>();
+        cam = GetComponentInChildren<Camera>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        TurnPlayer();
+        
+    }
+    void TurnPlayer()
+    {
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h * mouseSensitivityX, 0);
+
+        cameraPitch += v * mouseSensitivityY;
+
+        if (cameraPitch < -80) cameraPitch = -80;
+        if (cameraPitch > 80) cameraPitch = 80;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, 0, 0);
+
+
+        cam.transform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+
+    }
+    void MovePlayer()
+    {
+        //Get input: 
+
+        float v = Input.GetAxis("Vertical");// W+S /UP-DOWN : a value between -1 and 1
+        float h = Input.GetAxis("Horizontal"); // A+D / LEFT+RIGHT : a value between -1 and 1
+
+        Vector3 speed = (transform.right * h + transform.forward * v) * moveSpeed;
+        pawn.SimpleMove(speed); // simple move adds gravity
+    }
+}
